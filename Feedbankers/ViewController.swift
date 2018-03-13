c3841ef5d6712af33a3b501e16ef8c955275f1be
Rodrigo Guimaraes <rolongui@yahoo.com.br>
2018-03-13 16:12:53 -0300

    Initial Commit

---
+++ Feedbankers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Feedbankers
+//
+//  Created by Rodrigo Longhi Guimarães on 13/03/18.
+//  Copyright © 2018 Rodrigo Longhi Guimarães. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
