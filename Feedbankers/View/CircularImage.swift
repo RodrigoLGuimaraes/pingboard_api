8e820a2a98158702f063ba7f195c9b2742098a1e
Pedro Derkacz <pedro.kcz@gmail.com>
2018-03-14 18:00:05 -0300

    Fixed image with rounded corner

--- Feedbankers/View/CircularImage.swift
+++ Feedbankers/View/CircularImage.swift
@@ -11,7 +11,11 @@ import UIKit
 class CircularImage: UIImageView {
 
     override func awakeFromNib() {
-        self.layer.cornerRadius = self.bounds.width/2
+        self.layer.cornerRadius = self.frame.height/2.0
+        self.layer.masksToBounds = true
+        // POG to fix the corner radius
+        self.layer.borderWidth = 0.1
+        self.layer.borderColor = UIColor.white.cgColor
     }
-
+    
 }
---
+++ Feedbankers/View/CircularView.swift
@@ -0,0 +1,21 @@
+//
+//  CircularView.swift
+//  Feedbankers
+//
+//  Created by Pedro Marcos Derkacz on 14/03/18.
+//  Copyright © 2018 Rodrigo Longhi Guimarães. All rights reserved.
+//
+
+import UIKit
+
+class CircularView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
