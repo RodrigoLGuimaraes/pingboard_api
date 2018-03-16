9752e02c6195ee139bc24f6ab115631911c4cfc8
Rodrigo Longhi Guimarães <rodrigolg@EB120048.local>
2018-03-16 11:04:44 -0300

    save button for form

--- Feedbankers/Controllers/FeedbackFormViewController.swift
+++ Feedbankers/Controllers/FeedbackFormViewController.swift
@@ -11,22 +11,51 @@ import Eureka
 
 class FeedbackFormViewController: FormViewController {
     
+    var questions = ["Trabalha bem?", "Boa praça?", "Gente boa?"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        createForm(questions: ["Trabalha bem?", "Boa praça?", "Gente boa?"])
+        createForm()
         print(form.values())
     }
 
-    func createForm(questions:[String]){
+    func createForm(){
         questions.forEach { (question) in
             let section = Section(question)
             let row = SegmentedRow<String>(){
-                $0.options = ["Nope", "Maybe", "Always"]
-                $0.value = ""
+                $0.tag = question
+                $0.options = ["Não", "Às vezes", "Sempre"]
             }
             
             section <<< row
             form +++ section
         }
+        
+        let section = Section("")
+        let row = ButtonRow() {
+            $0.tag = "saveButton"
+            $0.title = "SAVE"
+            $0.value = "SAVE"
+            $0.onCellSelection({ (_, _) in
+                self.saveAndQuit()
+            })
+        }
+        
+        section <<< row
+        form +++ section
+        
+    }
+    
+    func saveAndQuit() {
+        for row in form.allRows {
+            for question in questions {
+                if row.tag! == question {
+                    //TODO: Save
+                    continue
+                }
+            }
+        }
+        
+        self.navigationController?.popViewController(animated: true)
     }
 }
